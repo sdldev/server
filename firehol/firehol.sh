cabff65aa0421bcc7cf265949dc54636e5fd72f8
sdldev <109410060+sdldev@users.noreply.github.com>
2022-08-04 01:44:27 +0700

    Create firehol.sh

---
+++ firehol/firehol.sh
@@ -0,0 +1,39 @@
+# cd somewhere
+cd /tmp
+
+# download iprange and firehol from github
+git clone https://github.com/firehol/iprange.git iprange.git
+git clone https://github.com/firehol/firehol.git firehol.git
+
+# install iprange
+cd iprange.git
+
+./autogen.sh
+# make sure it completed successfully
+
+./configure --prefix=/usr CFLAGS="-march=native -O3" --disable-man
+# make sure it completed successfully
+
+make
+# make sure it completed successfully
+
+sudo make install
+# make sure it completed successfully
+
+# install firehol
+cd ../firehol.git
+
+./autogen.sh
+# make sure it completed successfully
+
+./configure --prefix=/usr --sysconfdir=/etc --disable-man --disable-doc
+# make sure it completed successfully
+
+make
+# make sure it completed successfully
+
+sudo make install
+# make sure it completed successfully
+
+# Create the default RUN_PARENT_DIR='/usr/var/run' who is set in '/etc/firehol/update-ipsets.conf'
+mkdir -p /usr/var/run
